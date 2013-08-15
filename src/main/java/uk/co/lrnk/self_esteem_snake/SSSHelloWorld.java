e10151019c3d40eca39ed241bf056c56c3e33543
Lrnk <laurie.boyes@gmail.com>
2013-08-15 22:25:29 +0100

    Having a crack at cloning the repo and commiting from gitbash

--- src/main/java/uk/co/lrnk/self_esteem_snake/SSSHelloWorld.java
+++ src/main/java/uk/co/lrnk/self_esteem_snake/SSSHelloWorld.java
@@ -4,6 +4,7 @@ public class SSSHelloWorld {
 
     public static void main(String[] args) {
         System.out.println("SSSSup world, let's do this thing!");
+        System.out.println("And let's try a checkout and commit from gitbash. And then a bit more.");
     }
     
 }
