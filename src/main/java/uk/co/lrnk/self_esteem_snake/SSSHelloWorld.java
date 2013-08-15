06fbeed7b0c489cd03a1c603d2954b5738d5273f
Lrnk <laurie.boyes@gmail.com>
2013-08-15 21:49:23 +0100

    Project structure and hello. Whoops! Apparently Intellij Community edition doesn't autosave?

---
+++ src/main/java/uk/co/lrnk/self_esteem_snake/SSSHelloWorld.java
@@ -0,0 +1,9 @@
+package uk.co.lrnk.self_esteem_snake;
+
+public class SSSHelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("SSSSup world, let's do this thing!");
+    }
+
+}
