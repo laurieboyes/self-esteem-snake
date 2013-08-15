bbffd671ec6856b6c2652e78b9eb2dcf1cb79613
Lrnk <laurie.boyes@gmail.com>
2013-08-15 22:01:50 +0100

    Hopefully cleaning up some learning damage

--- src/main/java/uk/co/lrnk/self_esteem_snake/SSSHelloWorld.java
+++ src/main/java/uk/co/lrnk/self_esteem_snake/SSSHelloWorld.java
@@ -1,12 +1,9 @@
 package uk.co.lrnk.self_esteem_snake;
 
 public class SSSHelloWorld {
-<<<<<<< HEAD
 
     public static void main(String[] args) {
         System.out.println("SSSSup world, let's do this thing!");
     }
-
-=======
->>>>>>> d7ae9833a3e54e21d4dd536bbe1fbf883692df2c
+    
 }
